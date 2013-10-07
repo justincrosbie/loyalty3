dbcf7e5f9b2ef6f6f5a51cde1d82cfca8386be90
justincrosbie <justincrosbie@gmail.com>
2013-10-07 18:11:16 +1100

    loader

--- public/js/services/bookings.js
+++ public/js/services/bookings.js
@@ -10,15 +10,4 @@ window.angular.module('ngff.services.bookings', [])
           update: {method: 'PUT'}
         }
       )
-
-      $http({method: 'GET', url: '/someUrl'}).
-        success(function(data, status, headers, config) {
-          // this callback will be called asynchronously
-          // when the response is available
-        }).
-        error(function(data, status, headers, config) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-        });
-        
     }]);
